f1e8f6d9464c205b6d529137cf5a06d7633b69d6
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-03-11 13:05:30 +0200

    open build in edge

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+// Create gallery markup
+function createGalleryMarkup(arrayOfObjects) {
+  return arrayOfObjects
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+         `
+    )
+    .join('');
+}
+
+// Create and insert gallery markup
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createGalleryMarkup(galleryItems)
+);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
