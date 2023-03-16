dadc8b0ef1916733cfdbb74b7b3c388528b328ef
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-03-16 21:48:20 +0200

    Button disabled if one of the fields is empty

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,21 +4,36 @@ const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('input[name="email"]'),
   textarea: document.querySelector('textarea[name="message"]'),
+  button: document.querySelector('button[type="submit"]'),
 };
+
+// Disable 'Submit' button...
+// refs.button.setAttribute('disabled', true);
+
+// ...until all required fields have been filled
+function checkIfAllFieldsWereFilled() {
+  if (refs.input.value !== '' && refs.textarea.value !== '') {
+    refs.button.disabled = false;
+    return;
+  }
+  refs.button.disabled = true;
+}
 
 const savedInput = localStorage.getItem('feedback-form-state');
 const parsedSavedInput = JSON.parse(savedInput);
 
 // Check for saved data in 'localStorage'
 if (savedInput) {
-  // Prepolulate form fields with data from 'localStorage'
+  // Pre-populate form fields with data from 'localStorage'
   refs.input.value = parsedSavedInput.email;
   refs.textarea.value = parsedSavedInput.message;
+  checkIfAllFieldsWereFilled();
 }
 
 // Create a temp object for storing form data
 let tempInput = parsedSavedInput || {};
 
+// Listen to user input in form fields
 refs.form.addEventListener('input', throttle(onInput, 500));
 
 function onInput(event) {
@@ -28,6 +43,7 @@ function onInput(event) {
   tempInput[name] = value;
 
   localStorage.setItem('feedback-form-state', JSON.stringify(tempInput));
+  checkIfAllFieldsWereFilled();
 }
 
 // Submitting the form
@@ -35,8 +51,11 @@ refs.form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
   event.preventDefault();
+
   console.log('User submitted the following data:', parsedSavedInput);
   localStorage.removeItem('feedback-form-state');
+  // Reset fom fields
   event.currentTarget.reset();
+  // Clear temp object
   tempInput = {};
 }
